d0bf9378a59072a8a59484ab3fad63b8430b2a03
Raymel Francisco <franciscoraymel@gmail.com>
2017-03-28 00:11:45 +0800

    Completed lexer, used regex instead

--- lexer.go
+++ lexer.go
@@ -1,91 +1,74 @@
 
 package main
 
-import "fmt"
-import "unicode"
+import (
+	"fmt"
+	"regexp"
+)
 
 var line, checker, size int = 1, 0, 0
-var src string
+var src string = 
+`
+'_s = "str"
+#ien87 = -87
+##dbl_89 = -8.9
+?blbltrue = /
+?blblfalse = X
+^{
+}
+`
 
 func main() {
-	fmt.Println("hho")
 
-	src = "_huehue"
+	fmt.Println("Lexer start!")
 	size = len(src) - 1
 
-	fmt.Println( IsIdentifier() )
+	// identifiers
+	re := regexp.MustCompile("\\s+(##|#|'|\\?)[_a-zA-Z][_a-zA-Z0-9]*\\s+")
+	fmt.Printf("identifiers: %q\n", re.FindAllString(src, size) )
+
+	// integers
+	re = regexp.MustCompile("\\s+-*[0-9]+.[0-9]+\\s+")
+	fmt.Printf("integers: %q\n" , re.FindAllString(src, size) )
+
+	// doubles
+	re = regexp.MustCompile("\\s+-*[0-9]*\\s+")
+	fmt.Printf("doubles: %q\n" , re.FindAllString(src, size) )
+	
+	// strings
+	re = regexp.MustCompile("\\s+\".*\"")
+	fmt.Printf("strings: %q\n" , re.FindAllString(src, size) )
+
+	// bools
+	re = regexp.MustCompile("\\s+[/X]\\s+")
+	fmt.Printf("bools: %q\n" , re.FindAllString(src, size) )
+
+	// first level delimiters: commas, arrays, parenthesis
+	re = regexp.MustCompile("(\\[|\\]|\\(|\\)|,)")
+	fmt.Printf("first level delimiters: %q\n" , re.FindAllString(src, size) )
+
+	// second level delimiters: logic levels
+	re = regexp.MustCompile("(}|\\^{|\\?\\?\\?{|\\?\\?{|\\?{)")
+	fmt.Printf("second level delimiters: %q\n" , re.FindAllString(src, size) )
+
+	// assignment operators
+	re = regexp.MustCompile("\\s+(\\+|\\+=|-=|\\*=)\\s+")
+	fmt.Printf("assignment operators: %q\n" , re.FindAllString(src, size) )
+
+	// increment operators
+	re = regexp.MustCompile("\\s+(\\+\\+|--|\\*\\*)\\s+")
+	fmt.Printf("increment operators: %q\n" , re.FindAllString(src, size) )
+
+	// arithmetic operators
+	re = regexp.MustCompile("\\s+(\\+|-|\\*|/|%|\\^)\\s+")
+	fmt.Printf("arithmetic operators: %q\n" , re.FindAllString(src, size) )
+
+	// relational operators
+	re = regexp.MustCompile("\\s+(>|<|<=|>=|==|!=)\\s+")
+	fmt.Printf("relational operators: %q\n" , re.FindAllString(src, size) )
+
+	// logical operators
+	re = regexp.MustCompile("\\s+(\\|\\||&&|!)\\s+")
+	fmt.Printf("relational operators: %q\n" , re.FindAllString(src, size) )
 
 }
-
-func CurrentChar() (byte) {
-	if checker <= size {
-		return src[checker]
-	}
-	return 0
-}
-
-func StringAt(start, end int) (string) {
-	if checker <= size {
-		return string(src[start:end])
-	}
-	return ""
-}
-
-func IsSingleComment() string {
-	if CurrentChar() == '~'  {
-		for {
-			checker++
-			if CurrentChar() == '\n' || checker == size {
-				line++
-				return "SINGLE-COMMENT"
-			}
-		}
-	}
-	return ""
-}
-
-func IsMultiComment() string {
-	if StringAt(checker, checker + 2) == "::" {
-		checker++
-
-		for {
-			if CurrentChar() == '\n' {
-				line++
-			}
-
-			checker++
-
-			if StringAt(checker, checker + 2) == "::" {
-				checker++
-				return "MULTI-COMMENT"
-			}
-		}
-	}
-	return ""
-}
-
-func IsIdentifier() string {
-	if 	CurrentChar() == '_' || 
-		unicode.IsLetter(rune(CurrentChar())) {
-
-		token := string(CurrentChar())
-		checker++
-
-		for CurrentChar() == '_' ||
-			unicode.IsLetter(rune(CurrentChar())) || 
-			unicode.IsNumber(rune(CurrentChar())) {
-
-			token += string(CurrentChar())
-			checker++
-		}
-		checker--
-		return string(token)
-	}
-	return ""
-}
-
-// func IsInteger() string {
-// 	if 	unicode.IsLetter(rune(CurrentChar())) ||
-// 		unicode.IsLetter()
-// }
-
